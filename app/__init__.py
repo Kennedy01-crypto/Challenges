fc5deb037c003f74fd4df20afb34d82380e51f78
Jim Kennedy <kennedyomata@gmail.com>
2025-07-24 15:29:51 +0300

    renamed config_Class to config_class for better code readability and consistency

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,20 @@ from flask import Flask
 from config import Config
 import os
 
-def create_app(config_Class = Config):
+def create_app(config_class=Config):
     app = Flask(__name__, instance_relative_config=True)
-
-    # load configuration
-    app.config.from_object(config_Class)
-    app.config.from_pyfile('config.py', silent=True)
-
+    
+    # Load configuration
+    app.config.from_object(config_class)
+    app.config.from_pyfile('config.py', silent=True) # from instance folder
+ 
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # register blueprints, extensions, etc
+    # Register blueprints
     from app.routes import main_bp
     app.register_blueprint(main_bp)
 
